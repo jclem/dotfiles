029db46a44d9e9f6dc6504f64318abd96478ebff
Jonathan Clem <j@jclem.me>
2023-09-27 13:57:51 -0400

    Install fisher and fzf plugin

---
+++ dot_config/fish/conf.d/fzf2.fish
@@ -0,0 +1,19 @@
+# Named such that it runs after "fzf.fish" installed by plugin.
+#
+# fzf_configure_bindings --help
+#
+# COMMAND            |  DEFAULT KEY SEQUENCE         |  CORRESPONDING OPTION
+# Search Directory   |  Ctrl+Alt+F (F for file)      |  --directory
+# Search Git Log     |  Ctrl+Alt+L (L for log)       |  --git_log
+# Search Git Status  |  Ctrl+Alt+S (S for status)    |  --git_status
+# Search History     |  Ctrl+R     (R for reverse)   |  --history
+# Search Processes   |  Ctrl+Alt+P (P for process)   |  --processes
+# Search Variables   |  Ctrl+V     (V for variable)  |  --variables
+
+fzf_configure_bindings \
+    --directory=\cf \
+    --git_log=\cg \
+    --git_status=\cs \
+    --history=\ch \
+    --processes=\cp \
+    --variables=\cv
