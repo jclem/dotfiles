b951d15ebf7379adf39cbbbcb2df6f3594403850
Jonathan Clem <j@jclem.me>
2024-01-05 11:22:11 -0500

    Add Tailscale abbreviation

--- dot_config/fish/conf.d/03-abbreviations.fish
+++ dot_config/fish/conf.d/03-abbreviations.fish
@@ -2,5 +2,6 @@ if status --is-interactive
     abbr --add --global cz  chezmoi
     abbr --add --global fly flyctl
     abbr --add --global g   git
+    abbr --add --global ts  tailscale
     abbr --add --global vim nvim
 end
