0bcb13d47586312ea09925a0634949ea655a7baf
Jonathan Clem <jclem@magic.dev>
2024-03-18 17:01:11 -0400

    Add gpb abbreviation

--- dot_config/fish/conf.d/03-abbreviations.fish
+++ dot_config/fish/conf.d/03-abbreviations.fish
@@ -2,6 +2,7 @@ if status --is-interactive
     abbr --add --global cz  chezmoi
     abbr --add --global fly flyctl
     abbr --add --global g   git
+    abbr --add --global gpb "git push --set-upstream origin (current_branch)"
     abbr --add --global ts  tailscale
     abbr --add --global vim nvim
 end
