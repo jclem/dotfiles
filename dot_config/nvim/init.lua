bde03112c97d45c5cd4cde2766b280fffcab8d07
Jonathan Clem <jclem@magic.dev>
2024-03-19 12:36:35 -0400

    Rearrange nvim, add some new LSPs

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,3 +1,74 @@
+vim.g.mapleader = " " -- Use space for mapleader, very efficient.
+vim.g.maplocalleader = " "
+
+-- # Disable netrw
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- # Settings
+
+-- ## Basic Settings
+vim.o.termguicolors = true -- Enable 24-bit RGB colors
+vim.o.number = true -- Show line numbers
+vim.o.showmatch = true -- Show matching brackets
+vim.o.showmode = false -- Don't show mode, will be in status line
+vim.o.splitright = true -- Open new splits to the right
+vim.o.splitbelow = true -- Open new splits to the bottom
+vim.o.autowrite = true -- Automatically write before running commands
+vim.o.mouse = "a" -- Enable mouse support
+vim.o.clipboard = "unnamedplus" -- Use system clipboard
+vim.o.swapfile = false -- Disable swap files
+vim.o.filetype = "on" -- Enable filetype detection
+vim.o.signcolumn = 'yes' -- Always show sign column
+vim.o.updatetime = 250 -- Decrease from default (4s)
+vim.o.timeoutlen = 300 -- Decrease from default (1s)
+vim.o.inccommand = 'split' -- Show live preview of substitutions
+vim.o.cursorline = true -- Highlight current line
+vim.o.scrolloff = 10 -- Keep 10 lines above and below the cursor
+
+-- ## Search Settings
+vim.o.ignorecase = true -- Ignore case when searching
+vim.o.smartcase = true -- ...except when non-lowercase characters are used
+vim.o.completeopt = "menuone,noinsert,noselect" -- Completion options
+vim.o.hlsearch = true -- Highlight search results
+
+-- ## Undo Settings
+vim.o.undofile = true -- Enable undo history
+vim.o.undodir = vim.fn.stdpath("data") .. "undo" -- Set undo directory
+
+-- ## Indentation Settings
+vim.o.breakindent = true -- Enable indented hard breaks
+vim.o.expandtab = true -- Use tabs instead of spaces, except when otherwise configured
+vim.o.shiftwidth = 2 -- Number of spaces to use for autoindent
+vim.o.softtabstop = 2 -- Number of spaces that a <Tab> counts for while performing editing operations
+vim.o.smarttab = true -- Use shiftwidth for tabbing with <Tab> and <BS>
+vim.o.tabstop = 2 -- Number of spaces that a <Tab> counts for
+vim.o.autoindent = true -- Copy indent from current line when starting a new line
+vim.o.wrap = true -- Wrap lines
+
+-- ## Whitespace Settings
+vim.opt.list = true
+vim.opt.listchars = { trail = '·', nbsp = '␣' }
+
+-- # Keybindings
+
+-- ## Basic Keybindings
+vim.keymap.set("n", "<C-c>", "<cmd>nohlsearch<CR>") -- Clear search highlights with <Esc>
+
+-- ## Command Mode Keybindings
+vim.api.nvim_set_keymap("n", ";", ":", { noremap = true }) -- Use ; to enter command mode
+vim.api.nvim_set_keymap("v", ";", ":", { noremap = true }) -- Use ; to enter command mode
+
+-- ## Split Navigation Keybindings
+vim.keymap.set("", "<C-j>", "<C-w>j", { noremap = true }) -- Move down a split
+vim.keymap.set("", "<C-k>", "<C-w>k", { noremap = true }) -- Move up a split
+vim.keymap.set("", "<C-h>", "<C-w>h", { noremap = true }) -- Move left a split
+vim.keymap.set("", "<C-l>", "<C-w>l", { noremap = true }) -- Move right a split
+
+-- ## Miscellanous Keybindings
+vim.keymap.set({"n", "v"}, "\\", "<cmd>NvimTreeToggle<cr>", { noremap = true }) -- Toggle file tree
+
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not vim.loop.fs_stat(lazypath) then
@@ -78,7 +149,8 @@ require("lazy").setup({
       require("which-key").register({
         f = {
           name = "File",
-          w = { "<cmd>w<cr>", "Save file" },
+          s = { "<cmd>w<cr>", "Save file" },
+          S = { "<cmd>wall<cr>", "Save all files" },
         },
         s = {
           name = "Split",
@@ -162,6 +234,9 @@ require("lazy").setup({
     "neovim/nvim-lspconfig",
     config = function()
       require("lspconfig").astro.setup({})
+      require("lspconfig").golangci_lint_ls.setup({})
+      require("lspconfig").gopls.setup({})
+      require("lspconfig").ruby_ls.setup({})
       require("lspconfig").tsserver.setup({})
       require("lspconfig").elixirls.setup({
         cmd = { "/opt/elixir-ls/language_server.sh" },
@@ -201,51 +276,6 @@ require("lazy").setup({
     end,
   },
 })
-
--- Disable netrw
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
-vim.o.termguicolors = true -- Enable 24-bit RGB colors
-vim.o.number = true -- Show line numbers
-vim.o.showmatch = true -- Show matching brackets
-vim.o.splitright = true -- Open new splits to the right
-vim.o.splitbelow = true -- Open new splits to the bottom
-vim.o.autowrite = true -- Automatically write before running commands
-vim.o.mouse = "a" -- Enable mouse support
-vim.o.clipboard = "unnamedplus" -- Use system clipboard
-vim.o.swapfile = false -- Disable swap files
-vim.o.filetype = "on" -- Enable filetype detection
-
--- Search settings
-vim.o.ignorecase = true -- Ignore case when searching
-vim.o.smartcase = true -- ...except when non-lowercase characters are used
-vim.o.completeopt = "menuone,noinsert,noselect" -- Completion options
-
--- Undo settings
-vim.o.undofile = true -- Enable undo history
-vim.o.undodir = vim.fn.stdpath("data") .. "undo" -- Set undo directory
-
--- Indentation settings
-vim.o.expandtab = true -- Use tabs instead of spaces, except when otherwise configured
-vim.o.shiftwidth = 2 -- Number of spaces to use for autoindent
-vim.o.softtabstop = 2 -- Number of spaces that a <Tab> counts for while performing editing operations
-vim.o.smarttab = true -- Use shiftwidth for tabbing with <Tab> and <BS>
-vim.o.tabstop = 2 -- Number of spaces that a <Tab> counts for
-vim.o.autoindent = true -- Copy indent from current line when starting a new line
-vim.o.wrap = true -- Wrap lines
-
--- Command mode settings
-vim.g.mapleader = " " -- Use space for mapleader, very efficient.
-vim.api.nvim_set_keymap("n", ";", ":", { noremap = true }) -- Use ; to enter command mode
-vim.api.nvim_set_keymap("v", ";", ":", { noremap = true }) -- Use ; to enter command mode
-
--- Split navigation
-vim.keymap.set("", "<C-j>", "<C-w>j", { noremap = true }) -- Move down a split
-vim.keymap.set("", "<C-k>", "<C-w>k", { noremap = true }) -- Move up a split
-vim.keymap.set("", "<C-h>", "<C-w>h", { noremap = true }) -- Move left a split
-vim.keymap.set("", "<C-l>", "<C-w>l", { noremap = true }) -- Move right a split
-vim.keymap.set({"n", "v"}, "\\", "<cmd>NvimTreeToggle<cr>", { noremap = true }) -- Toggle file tree
 
 vim.api.nvim_create_autocmd("BufWritePre", {
   group = vim.api.nvim_create_augroup("format", { clear = true }),
