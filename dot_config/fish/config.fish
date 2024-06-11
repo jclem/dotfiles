dac8fd963e1d2f658a76a276bc563745b301f353
Jonathan Clem <jclem@magic.dev>
2024-06-11 15:30:56 -0400

    Source asdf early

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -7,5 +7,9 @@ set fish_cursor_insert line
 set fish_cursor_replace_one underscore
 set fish_cursor_visual block
 
+if test -f /opt/homebrew/opt/asdf/libexec/asdf.fish
+    source /opt/homebrew/opt/asdf/libexec/asdf.fish
+end
+
 function fish_mode_prompt
 end
