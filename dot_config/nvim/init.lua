cc7b6b247760b39873203df9fe6b32946b44f765
Jonathan Clem <jclem@magic.dev>
2024-06-12 10:37:15 -0400

    nvim: Add neoscroll.nvim

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -285,6 +285,12 @@ require("lazy").setup({
       require("telescope").load_extension("ui-select")
     end,
   },
+  {
+      "karb94/neoscroll.nvim",
+      config = function()
+          require('neoscroll').setup({})
+      end
+  },
 })
 
 vim.api.nvim_create_autocmd("BufWritePre", {
