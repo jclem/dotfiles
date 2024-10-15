f7caf7c5af864254c11598efee0fc92568b51065
Jonathan Clem <j@jclem.me>
2024-10-15 17:04:22 -0400

    Ensure /usr/local/bin is always in PATH

--- dot_config/fish/conf.d/01-path.fish
+++ dot_config/fish/conf.d/01-path.fish
@@ -2,6 +2,7 @@ if test (uname) = Darwin
     fish_add_path --move \
         $HOME/bin \
         $HOME/go/bin \
+        /usr/local/bin \
         /opt/homebrew/bin \
         /opt/homebrew/opt/ruby/bin
 end
