cfc05376670feb80df17ae4f635186eba298ea4b
Jonathan Clem <j@jclem.me>
2024-12-04 15:39:20 -0500

    Add uid abbreviation

--- dot_config/fish/conf.d/03-abbreviations.fish
+++ dot_config/fish/conf.d/03-abbreviations.fish
@@ -5,6 +5,7 @@ if status --is-interactive
     abbr --add --global g    git
     abbr --add --global gpb  "git push --set-upstream origin (current_branch)"
     abbr --add --global ts   tailscale
+    abbr --add --global uid  "mkid uuid 7 -b62"
     abbr --add --global vim  nvim
     abbr --add --global w    wezterm
 end
