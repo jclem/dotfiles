f39bb295cc18a60a9b20b1c146014fb669107904
Jonathan Clem <j@jclem.me>
2024-12-31 19:47:54 -0500

    Re-enable GitHub Copilot with <C-c> fix

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -71,6 +71,7 @@ vim.keymap.set("", "<C-l>", "<C-w>l", { noremap = true }) -- Move right a split
 
 -- ## Miscellanous Keybindings
 vim.keymap.set({ "n", "v" }, "\\", "<cmd>NvimTreeToggle<cr>", { noremap = true }) -- Toggle file tree
+vim.api.nvim_set_keymap("i", "<C-c>", "<Esc>", { noremap = true })                -- Map <C-c> to <Esc>, since <C-c> blocks autocommands # https://bsky.app/profile/tpo.pe/post/3lend4ayck22i
 
 require("config.lazy")
 
--- dot_config/nvim/lua/plugins/blink.lua
+++ dot_config/nvim/lua/plugins/blink.lua
@@ -7,37 +7,37 @@ return {
 		"sources.default",
 		"sources.providers",
 	},
-	event = "InsertEnter",
+	-- event = "InsertEnter",
 	opts = {
-		completion = {
-			accept = {
-				auto_brackets = {
-					enabled = true,
-				},
-			},
-			documentation = {
-				auto_show = true,
-				auto_show_delay_ms = 100,
-				window = { border = "rounded", },
-			},
-			ghost_text = {
-				enabled = true,
-			},
-			keyword = {
-				range = 'full',
-				regex = '[-_]\\|\\k',
-			},
-			menu = {
-				auto_show = true,
-				draw = {
-					treesitter = { "lsp", },
-				},
-			},
-			trigger = {
-				prefetch_on_insert = true,
-				show_on_blocked_trigger_characters = {},
-			},
-		},
+		-- 	completion = {
+		-- 		accept = {
+		-- 			auto_brackets = {
+		-- 				enabled = true,
+		-- 			},
+		-- 		},
+		-- 		documentation = {
+		-- 			auto_show = true,
+		-- 			auto_show_delay_ms = 100,
+		-- 			window = { border = "rounded", },
+		-- 		},
+		-- 		ghost_text = {
+		-- 			enabled = true,
+		-- 		},
+		-- 		keyword = {
+		-- 			range = 'full',
+		-- 			regex = '[-_]\\|\\k',
+		-- 		},
+		-- 		menu = {
+		-- 			auto_show = true,
+		-- 			draw = {
+		-- 				treesitter = { "lsp", },
+		-- 			},
+		-- 		},
+		-- 		trigger = {
+		-- 			prefetch_on_insert = true,
+		-- 			show_on_blocked_trigger_characters = {},
+		-- 		},
+		-- 	},
 		keymap = {
 			preset = "super-tab",
 		},
