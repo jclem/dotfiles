5c58469c39b4f99b33dbd7e7b5fcefaab8695f2c
Jonathan Clem <j@jclem.me>
2025-01-02 00:42:23 -0500

    Add treesitter-context

---
+++ dot_config/nvim/lua/plugins/nvim-treesitter-context.lua
@@ -0,0 +1,23 @@
+return {
+	"nvim-treesitter/nvim-treesitter-context",
+	opts = function()
+		local tsc = require("treesitter-context")
+
+		require("snacks").toggle({
+			name = "Treesitter Context",
+			get = tsc.enabled,
+			set = function(state)
+				if state then
+					tsc.enable()
+				else
+					tsc.disable()
+				end
+			end
+		}):map("<leader>uc")
+
+		return {
+			mode = "cursor",
+			max_lines = 3,
+		}
+	end
+}
