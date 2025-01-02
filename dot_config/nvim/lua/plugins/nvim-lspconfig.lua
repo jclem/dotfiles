efc0dadae861cb8ee2247315e595e96d673636f6
Jonathan Clem <j@jclem.me>
2025-01-01 23:16:34 -0500

    Enable inlay hints

--- dot_config/nvim/lua/plugins/nvim-lspconfig.lua
+++ dot_config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,10 +1,25 @@
 return {
-	"neovim/nvim-lspconfig",
-	config = function(_, opts)
-		local lsp = require("lspconfig")
+	{
+		"neovim/nvim-lspconfig",
+		version = "*",
+		config = function(_, opts)
+			require("which-key").add({
+				{
+					"<leader>ui",
+					function()
+						local bufnr = vim.api.nvim_get_current_buf()
+						local enabled = vim.lsp.inlay_hint.is_enabled({ bufnr = bufnr })
+						vim.lsp.inlay_hint.enable(not enabled, { bufnr = bufnr })
+					end,
+					desc = "Toggle Inlay Hints"
+				},
+			})
 
-		for server, server_opts in pairs(opts.servers or {}) do
-			lsp[server].setup(server_opts.settings or {})
+			local lsp = require("lspconfig")
+
+			for server, server_opts in pairs(opts.servers or {}) do
+				lsp[server].setup(server_opts.settings or {})
+			end
 		end
-	end
+	},
 }
--- dot_config/nvim/lua/plugins/which-key.lua
+++ dot_config/nvim/lua/plugins/which-key.lua
@@ -12,49 +12,50 @@ return {
 
 		wk.add({
 			-- Diagnostics
-			{ "<leader>d",  group = "Diagnostics" },
+			{ "<leader>d",       group = "Diagnostics" },
 			-- Buffers
-			{ "<leader>b",  group = "Buffers" },
-			{ "<leader>bp", "<cmd>BufferLineTogglePin<cr>",      desc = "Toggle Pin" },
-			{ "<leader>bd", "<cmd>bdelete<cr>",                  desc = "Delete Buffer" },
-			{ "<S-h>",      "<cmd>BufferLineCyclePrev<cr>",      desc = "Previous Buffer" },
-			{ "<S-l>",      "<cmd>BufferLineCycleNext<cr>",      desc = "Next Buffer" },
-			{ "[b",         "<cmd>BufferLineCyclePrev<cr>",      desc = "Previous Buffer" },
-			{ "]b",         "<cmd>BufferLineCycleNext<cr>",      desc = "Next Buffer" },
-			{ "[B",         "<cmd>BufferLineMovePrev<cr>",       desc = "Move Buffer Left" },
-			{ "]B",         "<cmd>BufferLineMoveNext<cr>",       desc = "Move Buffer Right" },
+			{ "<leader>b",       group = "Buffers" },
+			{ "<leader>bp",      "<cmd>BufferLineTogglePin<cr>",           desc = "Toggle Pin" },
+			{ "<leader>bd",      "<cmd>bdelete<cr>",                       desc = "Delete Buffer" },
+			{ "<S-h>",           "<cmd>BufferLineCyclePrev<cr>",           desc = "Previous Buffer" },
+			{ "<S-l>",           "<cmd>BufferLineCycleNext<cr>",           desc = "Next Buffer" },
+			{ "[b",              "<cmd>BufferLineCyclePrev<cr>",           desc = "Previous Buffer" },
+			{ "]b",              "<cmd>BufferLineCycleNext<cr>",           desc = "Next Buffer" },
+			{ "[B",              "<cmd>BufferLineMovePrev<cr>",            desc = "Move Buffer Left" },
+			{ "]B",              "<cmd>BufferLineMoveNext<cr>",            desc = "Move Buffer Right" },
 			-- Splits
-			{ "<leader>-",  "<cmd>split<cr>",                    desc = "Split (below)" },
-			{ "<leader>|",  "<cmd>vsplit<cr>",                   desc = "Split (right)" },
+			{ "<leader>-",       "<cmd>split<cr>",                         desc = "Split (below)" },
+			{ "<leader>|",       "<cmd>vsplit<cr>",                        desc = "Split (right)" },
 			-- Git
-			{ "<leader>g",  group = "Git" },
+			{ "<leader>g",       group = "Git" },
 			-- Files
-			{ "<leader>f",  group = "Files" },
-			{ "<leader>fs", "<cmd>w<cr>",                        desc = "Save" },
-			{ "<leader>fS", "<cmd>wall<cr>",                     desc = "Save All" },
-			{ "<leader>ft", "<cmd>NvimTreeFindFile<cr>",         desc = "Reveal in Tree" },
-			{ "<leader>fv", "<cmd>e!<cr>",                       desc = "Revert" },
+			{ "<leader>f",       group = "Files" },
+			{ "<leader>fs",      "<cmd>w<cr>",                             desc = "Save" },
+			{ "<leader>fS",      "<cmd>wall<cr>",                          desc = "Save All" },
+			{ "<leader>ft",      "<cmd>NvimTreeFindFile<cr>",              desc = "Reveal in Tree" },
+			{ "<leader>fv",      "<cmd>e!<cr>",                            desc = "Revert" },
 			-- LSP
-			{ "<leader>l",  group = "LSP" },
-			{ "<leader>lR", "<cmd>lua vim.lsp.buf.rename()<cr>", desc = "Rename" },
+			{ "<leader>l",       group = "LSP" },
+			{ "<leader>lR",      "<cmd>lua vim.lsp.buf.rename()<cr>",      desc = "Rename" },
 			-- UI
-			{ "<leader>u",  group = "UI" },
-			{ "<leader>uh", "<cmd>set hlsearch!<cr>",            desc = "Toggle Highlight" },
-			{ "<leader>ut", "<cmd>set cursorline!<cr>",          desc = "Toggle Cursorline" },
-			{ "<leader>uw", "<cmd>set wrap!<cr>",                desc = "Toggle Wrap" },
-			{ "<leader>ul", "<cmd>set list!<cr>",                desc = "Toggle List" },
+			{ "<leader>u",       group = "UI" },
+			{ "<leader>uh",      "<cmd>set hlsearch!<cr>",                 desc = "Toggle Highlight" },
+			{ "<leader>ut",      "<cmd>set cursorline!<cr>",               desc = "Toggle Cursorline" },
+			{ "<leader>uw",      "<cmd>set wrap!<cr>",                     desc = "Toggle Wrap" },
+			{ "<leader>ul",      "<cmd>set list!<cr>",                     desc = "Toggle List" },
 			-- Neovim
-			{ "<leader>v",  group = "Neovim" },
-			{ "<leader>vq", "<cmd>qa<cr>",                       desc = "Quit All" },
-			{ "<leader>vQ", "<cmd>qa!<cr>",                      desc = "Quit All (no warn)" },
-			{ "<leader>vW", "<cmd>wqa<cr>",                      desc = "Quit All (write)" },
+			{ "<leader>v",       group = "Neovim" },
+			{ "<leader>vq",      "<cmd>qa<cr>",                            desc = "Quit All" },
+			{ "<leader>vQ",      "<cmd>qa!<cr>",                           desc = "Quit All (no warn)" },
+			{ "<leader>vW",      "<cmd>wqa<cr>",                           desc = "Quit All (write)" },
 			-- Window
-			{ "<leader>w",  group = "Window" },
-			{ "<leader>wo", "<cmd>only<cr>",                     desc = "Only" },
+			{ "<leader>w",       group = "Window" },
+			{ "<leader>wo",      "<cmd>only<cr>",                          desc = "Only" },
 			-- Trouble
-			{ "<leader>x",  group = "Trouble" },
+			{ "<leader>x",       group = "Trouble" },
 			-- Etc
-			{ "<leader>/",  "<cmd>NvimTreeFocus<cr>",            desc = "Focus Tree" },
+			{ "<leader>/",       "<cmd>NvimTreeFocus<cr>",                 desc = "Focus Tree" },
+			{ "<leader><space>", "<cmd>lua vim.lsp.buf.code_action()<cr>", desc = "Code Actions" },
 		})
 	end
 }
