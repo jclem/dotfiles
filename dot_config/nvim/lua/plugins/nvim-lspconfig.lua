f0bcaf7ae98c34d998c87be2bb1529c0d03d3784
Jonathan Clem <j@jclem.me>
2025-01-07 11:30:35 -0500

    Remove unused shortcut

--- dot_config/nvim/lua/plugins/nvim-lspconfig.lua
+++ dot_config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -4,18 +4,6 @@ return {
 		version = "*",
 		enabled = true,
 		config = function(_, opts)
-			require("which-key").add({
-				{
-					"<leader>ui",
-					function()
-						local bufnr = vim.api.nvim_get_current_buf()
-						local enabled = vim.lsp.inlay_hint.is_enabled({ bufnr = bufnr })
-						vim.lsp.inlay_hint.enable(not enabled, { bufnr = bufnr })
-					end,
-					desc = "Toggle Inlay Hints"
-				},
-			})
-
 			local lsp = require("lspconfig")
 
 			for server, server_opts in pairs(opts.servers or {}) do
