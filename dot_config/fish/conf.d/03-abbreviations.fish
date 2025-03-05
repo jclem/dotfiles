aa57f362d40182a585c960e423e68c287f162b3d
Jonathan Clem <j@jclem.me>
2025-03-05 10:03:49 -0500

    Update fish abbreviations (kubectl/cursor)

--- dot_config/fish/conf.d/03-abbreviations.fish
+++ dot_config/fish/conf.d/03-abbreviations.fish
@@ -1,10 +1,11 @@
 if status --is-interactive
     abbr --add --global cat  bat
-    abbr --add --global code code-insiders
+    abbr --add --global code cursor
     abbr --add --global cz   chezmoi
     abbr --add --global fly  flyctl
     abbr --add --global g    git
     abbr --add --global gpb  "git push --set-upstream origin (current_branch)"
+    abbr --add --global k    kubectl
     abbr --add --global ts   tailscale
     abbr --add --global uid  "mkid uuid 7 -b62"
     abbr --add --global vim  nvim
