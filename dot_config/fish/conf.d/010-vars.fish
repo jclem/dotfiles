304c549e9648144b4536e410d2ab1ee4f51adbdc
Jonathan Clem <jonathan@jclem.net>
2025-06-12 16:54:33 -0400

    Change default editor from 'cursor' to 'nvim' in fish configuration

--- dot_config/fish/conf.d/010-vars.fish
+++ dot_config/fish/conf.d/010-vars.fish
@@ -1,6 +1,6 @@
 set -x CODE "$HOME/src/github.com"
 set -x DOWNLOADS "$HOME/Downloads"
-set -x EDITOR "cursor --wait"
+set -x EDITOR "nvim"
 set -x XDG_CACHE_HOME "$HOME/.cache"
 set -x XDG_CONFIG_HOME "$HOME/.config"
 set -x XDG_DATA_HOME "$HOME/.local/share"
