1ddffad20136001d925851dc8909f0623b669be2
Jonathan Clem <jonathan@jclem.net>
2025-06-16 22:17:17 -0400

    Add OBSIDIAN environment variable to Fish configuration

--- dot_config/fish/conf.d/010-vars.fish
+++ dot_config/fish/conf.d/010-vars.fish
@@ -1,6 +1,7 @@
 set -x CODE "$HOME/src/github.com"
 set -x DOWNLOADS "$HOME/Downloads"
 set -x EDITOR "nvim"
+set -x OBSIDIAN "$HOME/.obsidian"
 set -x XDG_CACHE_HOME "$HOME/.cache"
 set -x XDG_CONFIG_HOME "$HOME/.config"
 set -x XDG_DATA_HOME "$HOME/.local/share"
