82d599e3360a042cf76ecf2a7df15c0206372891
Jonathan Clem <jonathan@jclem.net>
2025-08-25 21:00:40 -0400

    Make r completion compatible with ls alias

--- dot_config/fish/completions/r.fish
+++ dot_config/fish/completions/r.fish
@@ -2,10 +2,10 @@
 
  complete --command r \
 	 --condition "test (count (commandline -opc)) -eq 1" \
-	 --arguments "(ls -L $CODE)" \
+	 --arguments "(command ls -L $CODE)" \
 	 --no-files
 
  complete --command r \
 	 --condition "test (count (commandline -opc)) -eq 2" \
-	 --arguments "(ls -L $CODE/(commandline -opc)[2])" \
+	 --arguments "(command ls -L $CODE/(commandline -opc)[2])" \
 	 --no-files
