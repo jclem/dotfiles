cb5f46ec5c1de94c4887a495aae415e852c645b8
Jonathan Clem <jonathan@jclem.net>
2025-09-18 10:23:11 -0400

    vim: Add biome fixes to TypeScript LSP

--- dot_config/nvim/lua/plugins/languages/typescript.lua
+++ dot_config/nvim/lua/plugins/languages/typescript.lua
@@ -46,4 +46,55 @@ return {
 			},
 		}
 	end,
+
+	init = function()
+		vim.api.nvim_create_autocmd("LspAttach", {
+			callback = function(args)
+				local client = vim.lsp.get_client_by_id(args.data.client_id)
+				if not client then
+					return
+				end
+
+				if client.name == "biome" then
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						group = vim.api.nvim_create_augroup("BiomeFixAll", {
+							clear = true,
+						}),
+
+						callback = function()
+							vim.lsp.buf.code_action({
+								context = {
+									---@diagnostic disable-next-line: assign-type-mismatch
+									only = { "source.fixAll.biome" },
+									diagnostics = {},
+								},
+
+								apply = true
+							})
+						end
+					})
+				end
+
+				if client.name == "eslint" then
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						group = vim.api.nvim_create_augroup("EslintFixAll", {
+							clear = true,
+						}),
+
+						callback = function()
+							vim.lsp.buf.code_action({
+								context = {
+									---@diagnostic disable-next-line: assign-type-mismatch
+									only = { "source.fixAll.eslint" },
+									diagnostics = {},
+								},
+
+								apply = true
+							})
+						end
+					})
+				end
+			end,
+		})
+	end
 }
