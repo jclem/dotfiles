115fc375b6901dbc7e425ea72ecd7dddfcea5333
Jonathan Clem <jonathan@jclem.net>
2025-10-28 10:13:54 -0400

    vim: Update schemastore config

--- dot_config/nvim/lua/plugins/languages/json.lua
+++ dot_config/nvim/lua/plugins/languages/json.lua
@@ -10,6 +10,15 @@ return {
 			local capabilities = vim.lsp.protocol.make_client_capabilities()
 			capabilities.textDocument.completion.completionItem.snippetSupport = true
 
+			vim.lsp.config("jsonls", {
+				settings = {
+					json = {
+						schemas = require("schemastore").json.schemas(),
+						validate = { enable = true },
+					}
+				}
+			})
+
 			vim.lsp.enable("jsonls")
 		end,
 	}
