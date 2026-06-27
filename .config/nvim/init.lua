1fe6bf09633d7ec1636951f696328a4ff358a0ee
Jonathan Clem <jclem@makenotion.com>
2026-06-26 23:45:59 -0400

    nvim: Activate Folio dark colorscheme

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,7 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+require("config.colorscheme")
 require("config.keymaps")
 require("config.plugins")
 require("config.lsp")
---
+++ .config/nvim/lua/config/colorscheme.lua
@@ -0,0 +1,4 @@
+-- Use the local Folio dark palette without automatic appearance switching.
+vim.o.termguicolors = true
+vim.o.background = "dark"
+vim.cmd.colorscheme("folio")
