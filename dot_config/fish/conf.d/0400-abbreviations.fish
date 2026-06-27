d3654c6e339b6b0b1e7052ecae5e0955d06b7b83
Jonathan Clem <jclem@makenotion.com>
2026-06-27 10:36:16 -0400

    fish: Add git push abbreviation

--- dot_config/fish/conf.d/0400-abbreviations.fish
+++ dot_config/fish/conf.d/0400-abbreviations.fish
@@ -1,7 +1,8 @@
 # Define command-line abbreviations only for interactive shells.
 if status --is-interactive
-    abbr --add --global cat  bat
-    abbr --add --global g    git
-    abbr --add --global vim  nvim
-    abbr --add --global zj   zellij
+    abbr --add --global cat bat
+    abbr --add --global g git
+    abbr --add --global gpb "git push --set-upstream origin (current_branch)"
+    abbr --add --global vim nvim
+    abbr --add --global zj zellij
 end
