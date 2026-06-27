d2633e07183ec67f8013447751d834af011fd107
Jonathan Clem <git@jclem.net>
2026-06-26 20:33:57 -0400

    fish: Add Mise dotfiles reload helper

---
+++ .config/fish/functions/reload.fish
@@ -0,0 +1,24 @@
+# Reapply the managed dotfiles with Mise bootstrap.
+function reload --description "Reapply dotfiles with Mise bootstrap"
+    argparse --name=reload --max-args=0 h/help -- $argv
+    or return
+
+    if set --query _flag_help
+        printf '%s\n' \
+            'Usage: reload [OPTIONS]' \
+            '' \
+            'Run Mise bootstrap and force managed dotfiles to' \
+            'replace their destinations.' \
+            '' \
+            'Options:' \
+            '  -h, --help  Show this help message'
+        return
+    end
+
+    if not command --search --quiet mise
+        printf '%s\n' 'reload: mise is required' >&2
+        return 127
+    end
+
+    command mise bootstrap --force-dotfiles
+end
