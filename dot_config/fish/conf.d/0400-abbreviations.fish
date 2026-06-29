e3605e05c85b79a7de22a4c0f326b18f5fe6ca02
Jonathan Clem <jclem@makenotion.com>
2026-06-28 20:49:42 -0400

    fish: Add lazygit abbreviation

--- dot_config/fish/conf.d/0400-abbreviations.fish
+++ dot_config/fish/conf.d/0400-abbreviations.fish
@@ -3,6 +3,7 @@ if status --is-interactive
     abbr --add --global cat bat
     abbr --add --global g git
     abbr --add --global gpb "git push --set-upstream origin (current_branch)"
+    abbr --add --global lg lazygit
     abbr --add --global pf pitchfork
     abbr --add --global vim nvim
     abbr --add --global zj zellij
